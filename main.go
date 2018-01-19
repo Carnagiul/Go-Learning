65b6c8a67e2913874e04e7663c50d0c1f1c78b62
Carnagiul-MAC <pierrequeruel@gmail.com>
2018-01-19 18:55:43 +0100

    Adding main with input command

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "os"
 
 func sum(x, y int) int {
 	return x + y
@@ -18,30 +19,112 @@ func div(x, y int) int {
 	return x / y
 }
 
-func mod(x int, y int) int {
+func mod(x, y int) int {
 	return x / y
 }
 
-func main() {
-	fmt.Println("Hello, 世界")
+func swap(x, y int) (int, int) {
+	return y, x
+}
+
+func signed(x int) int {
+	if x < 0 {
+		return -1
+	}
+	return 1
+}
+
+func biggerthan(x, y int) int {
+	if x < y {
+		return 0
+	}
+	return 1
+}
+
+func lessthan(x, y int) int {
+	if x > y {
+		return 0
+	}
+	return 1
+}
+
+func is_equal(x, y int) int {
+	if x == y {
+		return 1
+	}
+	return 0
+}
+
+func pow(x int) int {
+
+	neg := signed(x)
+	ret := 0
+	for ret = 0; x != 0; ret++ {
+		x = x / 10
+	}
+	return ret * neg
+}
+
+func testSum() {
 	fmt.Printf("Sum of 4 + 4 %d\n", sum(4, 4))
 	fmt.Printf("Sum of 4 + -4 %d\n", sum(4, -4))
 	fmt.Printf("Sum of -4 + -4 %d\n", sum(-4, -4))
 	fmt.Printf("Sum of -4 + 4 %d\n", sum(-4, 4))
+}
+
+func testSous() {
 	fmt.Printf("Sous of 4 - 4 %d\n", sous(4, 4))
 	fmt.Printf("Sous of 4 - -4 %d\n", sous(4, -4))
 	fmt.Printf("Sous of -4 - -4 %d\n", sous(-4, -4))
 	fmt.Printf("Sous of -4 - 4 %d\n", sous(-4, 4))
+}
+
+func testMul() {
 	fmt.Printf("mul of 4 * 4 %d\n", mul(4, 4))
 	fmt.Printf("mul of 4 * -4 %d\n", mul(4, -4))
 	fmt.Printf("mul of -4 * -4 %d\n", mul(-4, -4))
 	fmt.Printf("mul of -4 * 4 %d\n", mul(-4, 4))
+}
+
+func testDiv() {
 	fmt.Printf("Sum of 4 / 4 %d\n", div(4, 4))
 	fmt.Printf("Sum of 4 / -4 %d\n", div(4, -4))
 	fmt.Printf("Sum of -4 / -4 %d\n", div(-4, -4))
 	fmt.Printf("Sum of -4 / 4 %d\n", div(-4, 4))
+}
+
+func testMod() {
 	fmt.Printf("Mod of 4 %% 4 %d\n", mod(4, 4))
 	fmt.Printf("Mod of 4 %% -4 %d\n", mod(4, -4))
 	fmt.Printf("Mod of -4 %% -4 %d\n", mod(-4, -4))
 	fmt.Printf("Mod of -4 %% 4 %d\n", mod(-4, 4))
+}
+
+func testPow() {
+	fmt.Printf("Pow of 1 %d\n", pow(1))
+	fmt.Printf("Pow of 10 %d\n", pow(10))
+	fmt.Printf("Pow of -1 %d\n", pow(-1))
+	fmt.Printf("Pow of -10 %d\n", pow(-10))
+}
+
+func getArg() ([]string, int){
+	argv := os.Args
+	return argv, len(argv)
+}
+
+func displayArg(argv []string, argc int) {
+	for i := 0; i < argc; i++ {
+		fmt.Printf("argv[%d] = %s\n", i, argv[i])
+	}
+}
+
+func main() {
+	testSum()
+	testSous()
+	testMul()
+	testDiv()
+	testMod()
+	testPow()
+	//argv []string, argc int = getArg()
+	displayArg(getArg())
 }
