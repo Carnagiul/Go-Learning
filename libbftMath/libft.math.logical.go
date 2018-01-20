87217d511fa9dea18955587bf1697edf537d3e11
Carnagiul-portable <pierrequeruel@gmail.com>
2018-01-20 10:39:17 +0100

    Update LibftMath
    Adding Float gesture on libftMath

--- libbftMath/libft.math.logical.go
+++ libbftMath/libft.math.logical.go
@@ -3,6 +3,9 @@ package main
 type Integer struct {
 }
 
+type Float struct {
+}
+
 func (data *Integer)sum(a,b int) int {
 	return a + b
 }
@@ -54,6 +57,63 @@ func (data *Integer)get_pow(x int) int {
 }
 
 func (data *Integer)signed(x int) int {
+	if x < 0 {
+		return -1
+	}
+	return 1
+}
+
+func (data *Float)sum(a,b float64) float64 {
+	return a + b
+}
+
+func (data *Float)sous(a,b float64) float64 {
+	return a - b
+}
+
+func (data *Float)div(a,b float64) float64 {
+	return a / b
+}
+
+func (data *Float)mul(a,b float64) float64 {
+	return a / b
+}
+
+func (data *Float)mod(a,b float64) float64 {
+	return a / b
+}
+
+func (data *Float)biggerthan(x, y float64) int {
+	if x < y {
+		return 0
+	}
+	return 1
+}
+
+func (data *Float)lessthan(x, y float64) int {
+	if x > y {
+		return 0
+	}
+	return 1
+}
+
+func (data *Float)is_equal(x, y float64) int {
+	if x == y {
+		return 0
+	}
+	return 1
+}
+
+func (data *Float)get_pow(x float64) int {
+	neg := data.signed(x)
+	ret := 0
+	for ret = 0; x != 0; ret++ {
+		x = x / 10
+	}
+	return ret * neg
+}
+
+func (data *Float)signed(x float64) int {
 	if x < 0 {
 		return -1
 	}
